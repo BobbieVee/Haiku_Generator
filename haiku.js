17fb8ec085231f66e92cd6e141bbc6e0fc4d79c6
Robert van der Mark <robertvandermark@new-host.home>
2016-06-29 16:15:38 -0400

    created Word arr and syllable counting function

--- haiku.js
+++ haiku.js
@@ -1,13 +1,54 @@
 var fs = require('fs');
+
 // console.log(fs.readFileSync('./cmudict.txt'))
+
+
+
+
+
+var cmudictFile = readCmudictFile('./cmudict.txt');
+
+function readCmudictFile(file){
+  return fs.readFileSync(file).toString();
+}
+
+function formatData(data){    
+   var wordArr = [[],[],[],[],[],[],[],[],[],[]];
+   var syllables = '';
+   var lines = data.toString().split("\n"),
+       lineSplit
+   lines.forEach(function(line){    
+	    lineSplit = line.split("  ");
+	    var numberAccents = lineSplit[1].match(/\d/g);
+	    if  (numberAccents !== null && numberAccents !== undefined ){
+	    	// push word to element of wordArr with corresponding index
+
+
+	    	syllables = lineSplit[1].match(/\d/g).length
+	    	wordArr[syllables].push(lineSplit[0]); 
+	    } else {
+	    	syllables = "null"
+    }
+
+   
+    // console.log('\n Syllables =  ' + syllables)
+    	   
+    // console.log("The word " + lineSplit[0] + " has this phoneme    layout: " + lineSplit[1]); 
+    console.log(wordArr[2]);
+
+  });   
+}
+
+formatData(cmudictFile);
+
 var createHaiku = function createHaiku(structure){
     console.log("this should log a haiku with the structure " + structure);
+
+    formatData(cmudictFile);
     return structure;
 }
 
-// module.exports = {
-//   createHaiku: createHaiku,
-// };
+
 
 module.exports = createHaiku;
 
--- haiku_generator.js
+++ haiku_generator.js
@@ -1,4 +1,19 @@
+
 var createHaiku = require('./haiku');
-// var createHaiku = haiku.createHaiku;
-console.log(createHaiku(575))
+
+var filePath = process.argv[1];
+var structure = process.argv[2];
+
+//Create fileName from filePath.  Create error message.
+var fileName = filePath.split("/").slice(-1).toString();
+var noArgError = 'Error: Please submit a haiku structure as the first argument: \n       Ex: ' + fileName + ' 575 \n'
+
+// First argument 'structure' is mandatory
+if (structure === undefined){
+	return "bad"; 
+}
+
+
+
+console.log(createHaiku(structure))
 
