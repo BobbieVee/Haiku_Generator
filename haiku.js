6a1f590252c1a7eb4c1d32cdd6716bcaa5923983
Robert van der Mark <robertvandermark@new-host.home>
2016-06-30 09:52:02 -0400

    adding structure

--- haiku.js
+++ haiku.js
@@ -1,4 +1,5 @@
 var fs = require('fs');
+var wordArr = [[],[],[],[],[],[],[],[]];
 
 // console.log(fs.readFileSync('./cmudict.txt'))
 
@@ -13,39 +14,67 @@ function readCmudictFile(file){
 }
 
 function formatData(data){    
-   var wordArr = [[],[],[],[],[],[],[],[],[],[]];
+   
    var syllables = '';
    var lines = data.toString().split("\n"),
        lineSplit
    lines.forEach(function(line){    
 	    lineSplit = line.split("  ");
-	    var numberAccents = lineSplit[1].match(/\d/g);
+	    // console.log(lineSplit)
+	    if (lineSplit[1] !== undefined && lineSplit[1] !== null){
+	    	var numberAccents = lineSplit[1].match(/\d/g);
+	    }
+
+	    // push word to element of wordArr with corresponding index
 	    if  (numberAccents !== null && numberAccents !== undefined ){
-	    	// push word to element of wordArr with corresponding index
-
-
-	    	syllables = lineSplit[1].match(/\d/g).length
-	    	wordArr[syllables].push(lineSplit[0]); 
+	    	syllables = lineSplit[1].match(/\d/g).length;
 	    } else {
-	    	syllables = "null"
-    }
+	    	syllables = 0;
+    	}
 
-   
-    // console.log('\n Syllables =  ' + syllables)
-    	   
-    // console.log("The word " + lineSplit[0] + " has this phoneme    layout: " + lineSplit[1]); 
-    console.log(wordArr[2]);
+    	//Only use words with a max of 7 syllables 
+    	if (syllables < 8){
+    		wordArr[syllables].push(lineSplit[0]);
+    	}
+    	// return WordArr;
+	}); 
 
-  });   
+   function countSyllableWords(){
+   	for (var i=0; i < 8; i++){
+   		// console.log(i);
+   		var randNum = Math.random();
+   		console.log("Number of " + i + ' syllable words = ' + wordArr[i].length + " , first word is \'" + wordArr[i][0] + "\' and Random Word is " + wordArr[i][Math.floor(randNum * wordArr[i].length) ] ); 
+   	}
+   	
+
+   }
+   // countSyllableWords();
+  
+  	// console.log('2 syllable words = ' + wordArr[2].length + " , first word is \'" + wordArr[2][0] + "\'" );  
 }
 
-formatData(cmudictFile);
+// formatData(cmudictFile);
 
 var createHaiku = function createHaiku(structure){
     console.log("this should log a haiku with the structure " + structure);
 
     formatData(cmudictFile);
-    return structure;
+
+    function createLine(lineStructure){
+    	var finalLine = '';
+    	for(var k=0; k<lineStructure.length; k++){
+    		var randNum = Math.random();
+    		finalLine = wordArr[k][Math.floor(randNum * wordArr[k].length)] + ' ';
+    	}
+    	return finalLine.trim();
+    }
+
+    var finalHaiku = '';
+    for(var j=0; j<3; j++){
+    	finalHaiku += createLine(structure[j]) + "\n";
+    }
+
+    return finalHaiku;
 }
 
 
--- haiku_generator.js
+++ haiku_generator.js
@@ -1,6 +1,4 @@
 
-var createHaiku = require('./haiku');
-
 var filePath = process.argv[1];
 var structure = process.argv[2];
 
@@ -9,11 +7,23 @@ var fileName = filePath.split("/").slice(-1).toString();
 var noArgError = 'Error: Please submit a haiku structure as the first argument: \n       Ex: ' + fileName + ' 575 \n'
 
 // First argument 'structure' is mandatory
-if (structure === undefined){
-	return "bad"; 
-}
+function testArg(){
+	if (structure === undefined){
+		return noArgError; 
+	} else {
+		return ("  Structure specified for Haiku is " + structure);
+	}
+}	
+
+
+console.log(testArg());
+
+///Change input string Arg to Array.
+structure = structure.split('.')
 
 
 
-console.log(createHaiku(structure))
+
+var createHaiku = require('./haiku');
+console.log(createHaiku(structure));
 
